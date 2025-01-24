a16a996e4f20eb5d60701bd0444ef67a1f4df3a0
AlexTWillamette <adtinat@willamette.edu>
2025-01-24 15:10:11 -0800

    Completed Bag implementation

--- datastructures/bag.py
+++ datastructures/bag.py
@@ -4,25 +4,53 @@ from datastructures.ibag import IBag, T
 
 class Bag(IBag[T]):
     def __init__(self, *items: Optional[Iterable[T]]) -> None:
-        raise NotImplementedError("__init__ method not implemented")
+        self.__bagdict__ = {}
+        for item in items:
+            if item is None:
+                raise TypeError
+            else:
+                if item in self.__bagdict__.keys():
+                    self.__bagdict__[item] += 1
+                else:
+                    self.__bagdict__[item] = 1 
 
     def add(self, item: T) -> None:
-        raise NotImplementedError("add method not implemented")
+        if item is None:
+            raise TypeError
+        else:
+            if item in self.__bagdict__.keys():
+                    self.__bagdict__[item] += 1
+            else:
+                self.__bagdict__[item] = 1 
 
     def remove(self, item: T) -> None:
-        raise NotImplementedError("remove method not implemented")
+        if item in self.__bagdict__.keys():
+                self.__bagdict__[item] -= 1
+                if self.__bagdict__[item] == 0:
+                    self.__bagdict__.pop(item)
+        else:
+            raise ValueError
 
     def count(self, item: T) -> int:
-        raise NotImplementedError("count method not implemented")
+        if item in self.__bagdict__.keys():
+            return self.__bagdict__[item]
+        else:
+            return 0
 
     def __len__(self) -> int:
-        raise NotImplementedError("__len__ method not implemented")
+        totalcount = 0
+        for count in self.__bagdict__.values():
+            totalcount += count
+        return totalcount
 
     def distinct_items(self) -> int:
-        raise NotImplementedError("distinct_items method not implemented")
+        return self.__bagdict__.keys()
 
     def __contains__(self, item) -> bool:
-        raise NotImplementedError("__contains__ method not implemented")
+        if item in self.__bagdict__.keys():
+            return True
+        else:
+            return False
 
     def clear(self) -> None:
-        raise NotImplementedError("clear method not implemented")
+        self.__bagdict__ = {}
